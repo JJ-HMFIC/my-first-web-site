2de303573d7e2302bf86573ff169ac0b08bb4202
Jeon, Kyunghwan <114416829+JJ-HMFIC@users.noreply.github.com>
2023-03-27 12:21:20 +0900

    객체 파일 생성 23-03-27

---
+++ colors.js
@@ -0,0 +1,42 @@
+var Links = {
+    setColor : function(color)
+    {
+      var alist = document.querySelectorAll('a');
+      var i = 0;
+      while(i<alist.length)
+      {
+        alist[i].style.color = color;
+        i++;
+      }
+    }
+  }
+  var Body = {  // 객체
+    setColor : function (color)
+    {
+      document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor : function(color)
+    {
+      document.querySelector('body').style.backgroundColor =  color;
+    }
+  }
+  function nightDayHandler(self)
+  {
+    // 함수 내에서 this = 전역 객체 , 따라서 self 사용 
+    var target =  document.querySelector('body');
+    if(self.value === 'night')
+    {
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      self.value = 'day' ;
+      Links.setColor('powderblue');
+    }
+    else
+    {
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      self.value = 'night' ;
+      Links.setColor('blue');
+    }
+    
+  }
